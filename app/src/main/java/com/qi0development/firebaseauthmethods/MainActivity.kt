ba49c057da89aad7c906278ee9415c0547bdb49b
Weslley Santos <qi0development@gmail.com>
2021-02-21 23:37:38 -0300

    Initial commit

---
+++ app/src/main/java/com/qi0development/firebaseauthmethods/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.qi0development.firebaseauthmethods
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
